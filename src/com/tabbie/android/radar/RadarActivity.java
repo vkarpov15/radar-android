79c25f92a92f451d459c011308bc9cc661d09250
Justin Knutson <jknutson@alumni.princeton.edu>
2012-07-30 12:09:05 -0400

    Removed old code for server radar add/remove ping

--- src/com/tabbie/android/radar/RadarActivity.java
+++ src/com/tabbie/android/radar/RadarActivity.java
@@ -26,6 +26,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TabHost;
+import android.widget.Toast;
 import android.widget.TabHost.OnTabChangeListener;
 import android.widget.TabHost.TabSpec;
 import android.widget.TextView;
@@ -122,19 +123,6 @@ public class RadarActivity extends ServerThreadActivity implements
                 radarButton.setSelected(false);
               } else if (!e.isOnRadar() && commonController.addToRadar(e)) {
                 radarButton.setSelected(true);
-                // Now that we have our Facebook user info, we can send this to Tabbie to
-                // get our Tabbie id
-                runOnUiThread(new Runnable() {
-                  public void run() {
-                    ServerPostRequest req = new ServerPostRequest(
-                        ServerThread.TABBIE_SERVER + "/mobile/auth.json",
-                        MessageType.ADD_TO_RADAR,
-                        e.id);
-                    req.params.put("fb_token", facebook.getAccessToken());
-
-                    sendServerRequest(req);
-                  }
-                });
               }
               upVotes.setText(Integer.toString(e.radarCount));
               if (tabHost.getCurrentTab() != 2) {
