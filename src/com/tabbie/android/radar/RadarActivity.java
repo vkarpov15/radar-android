c19deb02d6ff927446efc8807d247e664b9330af
Justin Knutson <jknutson@alumni.princeton.edu>
2012-07-30 12:17:06 -0400

    Removed Matt's bull shit

--- src/com/tabbie/android/radar/RadarActivity.java
+++ src/com/tabbie/android/radar/RadarActivity.java
@@ -388,17 +388,6 @@ public class RadarActivity extends ServerThreadActivity implements
               .notifyDataSetChanged();
         }
       });
-    } else if (MessageType.ADD_TO_RADAR == resp.responseTo){
-    	Log.d("HI", "FUCK YOUR COUCH");
-	    this.runOnUiThread(new Runnable() {
-	        public void run() {
-	          ServerPostRequest req = new ServerPostRequest(
-	              ServerThread.TABBIE_SERVER + "/mobile/radar/" + "?auth_token="
-                    + token, MessageType.ADD_TO_RADAR);
-	
-	          sendServerRequest(req);
-	        }
-	      });    	
     }
     return false;
   }
