0091c0109a01c8912bf62ba0af0ae617469afd10
Valeri Karpov <valkar207@gmail.com>
2012-07-30 15:46:58 -0400

    Clean up ServerDeleteRequest - now more like ServerGetRequest

--- src/com/tabbie/android/radar/ServerDeleteRequest.java
+++ src/com/tabbie/android/radar/ServerDeleteRequest.java
@@ -3,39 +3,24 @@ package com.tabbie.android.radar;
 /**
  * ServerDeleteRequest.java
  * 
- * Created on: July 30, 2012
- * Author: vkarpov
+ * Created on: July 30, 2012 Author: vkarpov
  * 
  * A specialized HTTP DELETE ServerRequest.
  */
 
-import java.util.LinkedHashMap;
-
 public class ServerDeleteRequest extends ServerRequest {
-  public final LinkedHashMap<String, String> params = new LinkedHashMap<String, String>();
-
   public ServerDeleteRequest(String url, MessageType type) {
-    super(url, "DELETE", type);
-  }
-
-  @Override
-  public boolean hasOutput() {
-    return params.size() > 0;
+    super(url, "GET", type);
   }
 
   @Override
   public String getOutput() {
-    // TODO: URI encode text
-    if (0 == params.size()) {
-      return null;
-    }
-    String st = "";
-    for (String key : params.keySet()) {
-      if (st.length() > 0) {
-        st += "&";
-      }
-      st += key + "=" + params.get(key);
-    }
-    return st;
+    return null;
   }
+
+  @Override
+  public boolean hasOutput() {
+    return false;
+  }
+
 }
