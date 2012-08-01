01c6359b00d27e5163292e126758360e6afc67ac
Justin Knutson <jknutson@alumni.princeton.edu>
2012-08-01 16:54:34 -0400

    On second thought, left access to ListViews as private

--- src/com/tabbie/android/radar/RadarActivity.java
+++ src/com/tabbie/android/radar/RadarActivity.java
@@ -50,9 +50,9 @@ public class RadarActivity extends ServerThreadActivity implements
   private static final int MAX_TITLE_LENGTH = 36;
 
   private TabHost tabHost;
-  protected ListView featuredListView;
-  protected ListView allListView;
-  protected ListView radarListView;
+  private ListView featuredListView;
+  private ListView allListView;
+  private ListView radarListView;
   private TextView myNameView;
 
   private String token;
