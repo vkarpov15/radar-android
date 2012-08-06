1c3d53a42d293df6b250f4a43c0414019e2e5b11
Justin Knutson <jknutson@alumni.princeton.edu>
2012-08-05 20:55:25 -0400

    Debug commit, tabbieVirgin ALWAYS TRUE

--- src/com/tabbie/android/radar/RadarActivity.java
+++ src/com/tabbie/android/radar/RadarActivity.java
@@ -471,6 +471,12 @@ public class RadarActivity extends ServerThreadActivity implements
           findViewById(R.id.tonightlife_layout).setVisibility(View.VISIBLE);
           
           tabbieVirgin = getPreferences(MODE_PRIVATE).getBoolean("virgin", true);
+          tabbieVirgin = true; // TODO Delete this code after debugging
+          							/*
+          							 * SUPER
+          							 * IMPORTANT
+          							 * SHIT
+          							 */
           
           if(tabbieVirgin) {
 	          new AlertDialog.Builder(RadarActivity.this)
