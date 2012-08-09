c6f08afa9c183e34de88613156277072ba68dc3a
Justin Knutson <jknutson@alumni.princeton.edu>
2012-08-08 21:49:28 -0400

    Added ACRA libraries for debugging

---
+++ tonightlife-android/src/com/tabbie/android/radar/RadarApplication.java
@@ -0,0 +1,16 @@
+package com.tabbie.android.radar;
+
+import org.acra.*;
+import org.acra.annotation.*;
+
+@ReportsCrashes(formKey = "dE5zdFo3N1BkM2lnX1VTb1hVQXRnbXc6MQ")
+public class RadarApplication extends android.app.Application {
+	
+	@Override
+	public void onCreate() {
+		// The following line triggers the initialization of ACRA
+		ACRA.init(this);
+		super.onCreate();
+	}
+
+}
