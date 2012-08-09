18b819cb285e3279e7adb3a6cc32d9771ca6b9cd
Justin Knutson <jknutson@alumni.princeton.edu>
2012-08-09 17:52:40 -0400

    Edited file MemoryCache for ease of use

--- tonightlife-android/src/com/tabbie/android/radar/MemoryCache.java
+++ tonightlife-android/src/com/tabbie/android/radar/MemoryCache.java
@@ -1,5 +1,15 @@
 package com.tabbie.android.radar;
 
+/**
+ *  MemoryCache.java
+ *
+ *  Created on: August 9, 2012
+ *      Author: Fedor Vlasov, edited Justin Knutson
+ *      Source: https://github.com/thest1/LazyList/blob/master/src/com/fedorvlasov/lazylist/MemoryCache.java
+ *      
+ *  Data structure for loading and caching images
+ */
+
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
@@ -11,63 +21,91 @@ import android.util.Log;
 public class MemoryCache {
 
     private static final String TAG = "MemoryCache";
-    private Map<String, Bitmap> cache=Collections.synchronizedMap(
-            new LinkedHashMap<String, Bitmap>(10,1.5f,true));//Last argument true for LRU ordering
-    private long size=0;//current allocated size
-    private long limit=1000000;//max memory in bytes
+    private Map<String, Bitmap> cache = Collections.synchronizedMap(
+            new LinkedHashMap<String, Bitmap>(10, 1.5f, true));//Last argument true for LRU ordering
+    private long size = 0; //current allocated size
+    private long limit = 1000000; //max memory in bytes
 
-    public MemoryCache(){
+    public MemoryCache() {
         //use 25% of available heap size
         setLimit(Runtime.getRuntime().maxMemory()/4);
     }
     
-    public void setLimit(long new_limit){
-        limit=new_limit;
+    /**
+     * Set the maximum memory to be used for the cache
+     * @param New cache limit
+     */
+    public void setLimit(long newLimit) {
+        limit = newLimit;
         Log.i(TAG, "MemoryCache will use up to "+limit/1024./1024.+"MB");
     }
 
-    public Bitmap get(String id){
+    /**
+     * Restore a cached Bitmap
+     * @param id - The String identifier of the Bitmap to be restored
+     * @return The cached Bitmap or null if not present or an error is thrown
+     */
+    public Bitmap get(String id) {
         try{
             if(!cache.containsKey(id))
                 return null;
             //NullPointerException sometimes happen here http://code.google.com/p/osmdroid/issues/detail?id=78 
             return cache.get(id);
-        }catch(NullPointerException ex){
+        } catch(NullPointerException ex) {
+        	Log.e(TAG, "Caught an error, returning null");
             return null;
         }
     }
 
-    public void put(String id, Bitmap bitmap){
-        try{
-            if(cache.containsKey(id))
+    /**
+     * Add a new Bitmap to the cache or replace an old one
+     * @param id - The String key associated with this entry
+     * @param bitmap - The Bitmap to cache
+     */
+    public void put(String id, Bitmap bitmap) {
+        try {
+            if(cache.containsKey(id)) // Bitmap is about to be replaced, so
+            							// make sure we're not double counting
                 size-=getSizeInBytes(cache.get(id));
             cache.put(id, bitmap);
             size+=getSizeInBytes(bitmap);
             checkSize();
-        }catch(Throwable th){
+        } catch(Throwable th) {
             th.printStackTrace();
         }
     }
     
+    /**
+     * Helper method to maintain a cache size less than limit
+     * by removing least used old cached Bitmaps
+     */
     private void checkSize() {
         Log.i(TAG, "cache size="+size+" length="+cache.size());
-        if(size>limit){
-            Iterator<Entry<String, Bitmap>> iter=cache.entrySet().iterator();//least recently accessed item will be the first one iterated  
-            while(iter.hasNext()){
+        if(size > limit) {
+            Iterator<Entry<String, Bitmap>> iter=cache.entrySet().iterator(); //least recently accessed item will be the first one iterated  
+            while(iter.hasNext()) {
                 Entry<String, Bitmap> entry=iter.next();
                 size-=getSizeInBytes(entry.getValue());
                 iter.remove();
-                if(size<=limit)
+                if(size <= limit)
                     break;
             }
             Log.i(TAG, "Clean cache. New size "+cache.size());
         }
     }
 
+    /**
+     * Clear the cache
+     */
     public void clear() {
         cache.clear();
     }
 
+    /**
+     * Method to calculate the size of a given Bitmap
+     * @param bitmap - the Bitmap to size
+     * @return The number of bytes in the Bitmap
+     */
     long getSizeInBytes(Bitmap bitmap) {
         if(bitmap==null)
             return 0;
