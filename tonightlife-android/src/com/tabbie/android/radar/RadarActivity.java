dd039e54291d68513a3dc7a897647b7a0718942c
Justin Knutson <jknutson@alumni.princeton.edu>
2012-08-09 23:19:46 -0400

    Removed old inner class EventListAdapter

--- tonightlife-android/src/com/tabbie/android/radar/RadarActivity.java
+++ tonightlife-android/src/com/tabbie/android/radar/RadarActivity.java
@@ -83,134 +83,7 @@ public class RadarActivity extends ServerThreadActivity implements
   // FB junk
   private Facebook facebook = new Facebook("217386331697217");
   private SharedPreferences preferences;
-/*
-  protected class EventListAdapter extends ArrayAdapter<Event> {
-
-    public EventListAdapter(int resource, List<Event> events) {
-      super(RadarActivity.this, resource, R.layout.event_list_element, events);
-    }
-
-    @Override
-    public View getView(final int position,
-    		View convertView, final ViewGroup parent) {
-    	
-      if (null == convertView) {
-    	Log.i("EventListAdapter", "Instantiating a new list view element");
-        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        convertView = inflater.inflate(R.layout.event_list_element, null);
-      }
-      
-      final Event e = getItem(position);
-      
-      
-      // Populate TextViews with data
-      ((TextView) convertView
-    		  .findViewById(R.id.event_text))
-    		  .setText(e.name);
-
-      ((TextView) convertView
-    		  .findViewById(R.id.event_list_time))
-    		  .setText(e.time.makeYourTime());
-
-      ((TextView) convertView
-          .findViewById(R.id.event_location))
-          .setText(e.venueName);
-
-      ((TextView) convertView
-    		  .findViewById(R.id.upvotes))
-    		  .setText(Integer.toString(e.radarCount));
-
-      /*
-       * Check and see if there is an image that has been loaded If there is an
-       * image that has been loaded and it has been drawn, then do nothing If
-       * there is an image that has been loaded, but it hasn't been drawn, draw
-       * it If there is no image that has been loaded, display the loader and
-       * LOAD THAT SH*T
-       */
-/*
-      final ImageView loader = (ImageView) convertView
-          .findViewById(R.id.element_loader);
-      final ImageView img = (ImageView) convertView
-          .findViewById(R.id.event_image);
-
-      if (!remoteDrawableController.hasImage(e.image)) {
-        Log.d("RadarActivity", "Image still being retrieved, displaying loader");
-        loader.startAnimation(AnimationUtils.loadAnimation(RadarActivity.this,
-            R.anim.rotate));
-      } else if (img.getTag() == null
-          || 0 != ((URL) img.getTag()).toString().compareTo(e.image.toString())) {
-        Log.d("RadarActivity", "RDC has image");
-        loader.setVisibility(View.GONE);
-        img.setVisibility(View.VISIBLE);
-        remoteDrawableController.drawImage(e.image, img);
-      }
-
-      convertView.findViewById(R.id.list_list_element_layout)
-          .setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-              if (null != e) {
-                currentViewPosition = position;
-                ((Vibrator) getSystemService(Context.VIBRATOR_SERVICE))
-                    .vibrate(30);
-
-                new AsyncTask<Void, Void, Intent>() {
-
-                  private ProgressDialog dialog;
-
-                  @Override
-                  protected void onPreExecute() {
-                    dialog = ProgressDialog.show(RadarActivity.this, "",
-                        "Loading, please wait...");
-                    super.onPreExecute();
-                  }
-
-                  @Override
-                  protected Intent doInBackground(Void... params) {
-                    Intent intent = new Intent(RadarActivity.this,
-                        EventDetailsActivity.class);
-                    intent.putExtra("eventId", e.id);
-                    intent.putExtra("controller", commonController);
-                    intent.putExtra("image", remoteDrawableController.getAsParcelable(e.image));
-                    intent.putExtra("token", token);
-                    if (tabbieVirgin) {
-                      intent.putExtra("virgin", true); // Make sure this
-                                                       // activity knows it's in
-                                                       // tutorial mode
-                      tabbieVirgin = false; // User is no longer a prepubescent
-                                            // pussy
-                      getPreferences(MODE_PRIVATE).edit()
-                          .putBoolean("virgin", false).commit();
-                    }
-                    return intent;
-                  }
-
-                  @Override
-                  protected void onPostExecute(Intent result) {
-                    startActivityForResult(result,
-                        RadarCommonController.RETRIEVE_INSTANCE);
-                    dialog.dismiss();
-
-                  };
-                }.execute();
-
-              }
-            }
-          });
-
-      convertView.findViewById(R.id.location_image_layout).setOnClickListener(
-          new OnClickListener() {
-            public void onClick(View v) {
-              Intent intent = new Intent(RadarActivity.this,
-                  RadarMapActivity.class);
-              intent.putExtra("controller", commonController);
-              intent.putExtra("event", e);
-              startActivity(intent);
-            }
-          });
-      return convertView;
-    }
-  }
-*/
+  
   @Override
   public void onCreate(final Bundle savedInstanceState) {
     Log.d("RadarActivity", "OnCreate Method");
@@ -296,9 +169,7 @@ public class RadarActivity extends ServerThreadActivity implements
     tabHost.setOnTabChangedListener(this);
 
     featuredListView.setAdapter(new EventListAdapter(this, commonController.featuredList));
-
     allListView.setAdapter(new EventListAdapter(this, commonController.eventsList));
-
     radarListView.setAdapter(new EventListAdapter(this, commonController.radarList));
 
     findViewById(R.id.map_button).setOnClickListener(new OnClickListener() {
