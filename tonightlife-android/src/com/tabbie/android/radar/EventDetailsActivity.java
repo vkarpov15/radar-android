c3db77550eb39a154f8aaf4025599b3d10cf9eb1
Justin Knutson <jknutson@alumni.princeton.edu>
2012-08-12 20:53:52 -0400

    Delete commented code

--- tonightlife-android/src/com/tabbie/android/radar/EventDetailsActivity.java
+++ tonightlife-android/src/com/tabbie/android/radar/EventDetailsActivity.java
@@ -69,69 +69,6 @@ public class EventDetailsActivity extends ServerThreadActivity
     pager.setCurrentItem(commonController.eventsList.indexOf(e));
     
     /*
-    eventImage = (ImageView) findViewById(R.id.details_event_img);
-    eventImage.setImageBitmap(image);
-
-    ((TextView) findViewById(R.id.details_event_title)).setText(e.name);
-    ((TextView) findViewById(R.id.details_event_time)).setText(e.time
-        .makeYourTime());
-    ((TextView) findViewById(R.id.details_event_location)).setText(e.venueName);
-    ((TextView) findViewById(R.id.details_event_address)).setText(e.address);
-    ((TextView) findViewById(R.id.details_event_num_radar)).setText(Integer
-        .toString(e.radarCount));
-    ((TextView) findViewById(R.id.details_event_description))
-        .setText(e.description);
-    Linkify.addLinks((TextView) findViewById(R.id.details_event_description),
-        Linkify.WEB_URLS);
-    
-    ((ImageView) findViewById(R.id.location_image)).setOnClickListener(new OnClickListener() {
-		
-		@Override
-		public void onClick(View v) {
-		    Intent intent = new Intent(EventDetailsActivity.this,
-		            RadarMapActivity.class);
-		        intent.putExtra("controller", commonController);
-		        intent.putExtra("event", e);
-		        startActivity(intent);
-		}
-	});
-
-    final TextView radarCount = (TextView) findViewById(R.id.details_event_num_radar);
-    final ImageView radarButton = (ImageView) findViewById(R.id.add_to_radar_image);
-    radarButton.setSelected(e.isOnRadar());
-
-    radarButton.setOnClickListener(new OnClickListener() {
-      public void onClick(View v) {
-        if (e.isOnRadar() && commonController.removeFromRadar(e)) {
-          Log.v("EventDetailsActivity", "Removing event from radar");
-          radarButton.setSelected(false);
-          radarCount.setText(Integer.toString(e.radarCount));
-
-          ServerDeleteRequest req = new ServerDeleteRequest(
-              ServerThread.TABBIE_SERVER + "/mobile/radar/" + e.id
-                  + ".json?auth_token=" + token, MessageType.ADD_TO_RADAR);
-
-          serverThread.sendRequest(req);
-        } else if (!e.isOnRadar()) {
-          Log.v("EventDetailsActivity", "Adding event to radar");
-          if (commonController.addToRadar(e)) {
-            radarButton.setSelected(true);
-            radarCount.setText(Integer.toString(e.radarCount));
-
-            ServerPostRequest req = new ServerPostRequest(
-                ServerThread.TABBIE_SERVER + "/mobile/radar/" + e.id + ".json",
-                MessageType.ADD_TO_RADAR);
-            req.params.put("auth_token", token);
-            serverThread.sendRequest(req);
-          } else {
-            Toast.makeText(EventDetailsActivity.this,
-                "You can only add 3 events to your radar!", 5000).show();
-            return;
-          }
-        }
-      }
-    });
-
     if (tutorialMode) {
       tutorialController.showDetailsTutorial();
       tutorialMode = false;
