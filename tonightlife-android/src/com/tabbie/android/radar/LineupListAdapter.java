82cf650de21bb3b21d5335a635c4f913d77d9794
Justin Knutson <jknutson@alumni.princeton.edu>
2012-09-07 15:29:19 -0400

    Added new class LineupListAdapter

---
+++ tonightlife-android/src/com/tabbie/android/radar/LineupListAdapter.java
@@ -0,0 +1,14 @@
+package com.tabbie.android.radar;
+
+import java.util.List;
+
+import android.content.Context;
+
+public class LineupListAdapter extends EventListAdapter {
+
+	public LineupListAdapter(final Context context, final List<Event> events) {
+		super(context, events);
+		// TODO Auto-generated constructor stub
+	}
+
+}
