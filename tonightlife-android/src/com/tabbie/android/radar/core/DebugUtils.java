3640d3286a57c9e2b7bdeab6b77b7134bb164c07
Justin Knutson <jknutson@alumni.princeton.edu>
2012-09-16 00:25:15 -0400

    Moved BundleChecker to DebugUtils

--- tonightlife-android/src/com/tabbie/android/radar/core/DebugUtils.java
+++ tonightlife-android/src/com/tabbie/android/radar/core/DebugUtils.java
@@ -1,5 +1,23 @@
 package com.tabbie.android.radar.core;
 
-public class DebugUtils {
+import java.util.List;
+
+import android.os.Bundle;
+import android.util.Pair;
+
+public final class DebugUtils {
 	public static final boolean DEBUG = false;
+	
+  public static final boolean checkBundle(List<Pair<String, Class<?> > > expected, Bundle b) {
+    if (null == b) {
+      return false;
+    }
+    for (Pair<String, Class<?> > p : expected) {
+      if (!b.containsKey(p.first)) {
+        return false;
+      }
+      // TODO: Actual type checks
+    }
+    return true;
+  }
 }
