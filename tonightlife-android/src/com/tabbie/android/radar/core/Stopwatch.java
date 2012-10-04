35e263d34e69c852afca199eee1e51c5fd739ed6
Justin Knutson <jknutson@alumni.princeton.edu>
2012-10-04 16:13:06 -0400

    Slight cleanup in Stopwatch.java

--- tonightlife-android/src/com/tabbie/android/radar/core/Stopwatch.java
+++ tonightlife-android/src/com/tabbie/android/radar/core/Stopwatch.java
@@ -19,19 +19,7 @@ public class Stopwatch {
   private final Map<Long, Long> prev = new LinkedHashMap<Long, Long>();
   private final Map<Long, String> prevTag = new LinkedHashMap<Long, String>();
 
-  private boolean enabled = false;
   private long id = 0;
-
-  public Stopwatch() {
-  }
-
-  public void enable() {
-    enabled = true;
-  }
-
-  public void disable() {
-    enabled = false;
-  }
 
   public void init(String tag) {
     totals.put(tag, (long) 0);
