ff2fe4ca321236d9f9025fd249e09debfb4cd915
Justin Knutson <jknutson@alumni.princeton.edu>
2012-10-13 15:07:23 -0400

    New Sharing classes and xml

---
+++ tonightlife-android/src/com/tabbie/android/radar/SharingDialog.java
@@ -0,0 +1,13 @@
+package com.tabbie.android.radar;
+
+import android.app.Dialog;
+import android.content.Context;
+
+public class SharingDialog extends Dialog {
+
+	public SharingDialog(Context context) {
+		super(context);
+		setContentView(R.layout.sharing);
+	}
+
+}
