7da1177ad108c85fcc4a3d5132d71a004e20eff7
Justin Knutson <jknutson@alumni.princeton.edu>
2012-10-16 16:49:23 -0400

    Formatting in MessageType.java

--- tonightlife-android/src/com/tabbie/android/radar/enums/MessageType.java
+++ tonightlife-android/src/com/tabbie/android/radar/enums/MessageType.java
@@ -3,6 +3,13 @@ package com.tabbie.android.radar.enums;
 import com.tabbie.android.radar.http.ServerRequest;
 
 /**
+ * Revised: October 16, 2012
+ * @author Justin Knutson
+ * 
+ * Significant changes to the basic structure of this enum,
+ * including String variables for the different server requests
+ * and urls
+ * 
  *  MessageType.java
  * 
  *  Created on: June 21, 2012
@@ -14,20 +21,21 @@ import com.tabbie.android.radar.http.ServerRequest;
 public enum MessageType {
 	
 	// Post requests
-  TABBIE_LOGIN(ServerRequest.POST, "http://23.21.40.96/mobile/v1/auth.json"),
-  LOAD_FRIENDS(ServerRequest.POST, "http://23.21.40.96/mobile/test/friends.json"),
-  POST_MESSAGE(ServerRequest.POST, "http://23.21.40.96/mobile/test/gcm.json"),
+  TABBIE_LOGIN(ServerRequest.POST, 	"http://23.21.40.96/mobile/v1/auth.json"),
+  LOAD_FRIENDS(ServerRequest.POST, 	"http://23.21.40.96/mobile/test/friends.json"),
+  POST_MESSAGE(ServerRequest.POST, 	"http://23.21.40.96/mobile/test/gcm.json"),
   ADD_TO_LINEUP(ServerRequest.POST, "http://23.21.40.96/mobile/v1/radar/", ".json"),
   
   // Get requests
-  LOAD_EVENTS(ServerRequest.GET, "http://23.21.40.96/mobile/v1/all_authed.json?auth_token="),
+  LOAD_EVENTS(ServerRequest.GET, 		"http://23.21.40.96/mobile/v1/all_authed.json?auth_token="),
   FACEBOOK_LOGIN(ServerRequest.GET, "https://graph.facebook.com/me/?access_token="),
   
   // Put requests
-  REGISTER_GCM(ServerRequest.PUT, "http://23.21.40.96/mobile/test/gcm_key/", ".json?auth_token="),
+  REGISTER_GCM(ServerRequest.PUT, 	"http://23.21.40.96/mobile/test/gcm_key/", ".json?auth_token="),
   
   // Delete requests
-  REMOVE_FROM_LINEUP(ServerRequest.DELETE, "http://23.21.40.96/mobile/v1/radar/", ".json?auth_token=");
+  REMOVE_FROM_LINEUP(ServerRequest.DELETE,
+  																	"http://23.21.40.96/mobile/v1/radar/", ".json?auth_token=");
   
   public final String[] mUrl;
   public final String mType;
